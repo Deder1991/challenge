5d55717d65279784c838a3523d2eb48cead1cbf1
Deder1991 <l22619025@gmail.com>
2020-05-20 22:53:10 +0800

    增加老師的解答

--- second_highest.py
+++ second_highest.py
@@ -21,7 +21,7 @@ def second_highest(students):
 			n1 = 0 
 			n2 = 0
 	# print(students)
-	
+
 	n3 = 2 #開始比較的位置
 	print(students[1][0])
 	while True: 
@@ -32,6 +32,19 @@ def second_highest(students):
 			break
 
 second_highest(students)
+
+
+# def second_highest(students):
+#     grades = [s[1] for s in students] # 只把成績拿出來
+#     grades = sorted(grades, reverse=True)
+#     second = grades[1] # grades[0]是最高，grades[1]是第二高
+    
+#     # 再下來找誰是這個分數
+#     # 底下這句話的意思是拿到 所有分數跟第二高一樣的人的"人名"也就是s[0]的部分
+#     # 記得嗎 參數的這個students清單裡面的東西，s[0]是人名，s[1]是分數
+#     second_high_students = [s[0] for s in students if s[1] == second]
+#     for student in second_high_students:
+#         print(student)
 
 
 
